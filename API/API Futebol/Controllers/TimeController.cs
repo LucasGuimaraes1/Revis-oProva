2e4b17c096499e5e9e5b07de408d2f11388505df
LucasGuimaraes1 <63862235+LucasGuimaraes1@users.noreply.github.com>
2020-09-22 14:33:58 -0300

    Add files via upload

---
+++ API/API Futebol/Controllers/TimeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using A.P.I_Futebol.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace A.P.I_Futebol.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Listagem de todos os times");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok("Listagem de time específico");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Time time)
+        {
+            return Ok("Cadastro de time efetuado com sucesso");
+        }
+    }
+}
---
+++ API/API Futebol/Models/Time.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A.P.I_Futebol.Models
+{
+    public class Time
+    {
+        public int ID { get; set; }
+        public string Nome{ get; set; }
+        public int AnoCriação { get; set; }
+        public string Estado { get; set; }
+        public string Bandeira { get; set; }
+        public string Mascote { get; set; }
+        public int QtdeTitulosBR { get; set; }
+        public int QtdeTituloCopaBr { get; set; }
+        public int QtdeTituloRegional { get; set; }
+
+    }
+}
